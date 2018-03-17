eaddb4ff10b61eb1cd2373e2339687e2fe05f978
Daphne Link <daphnelink@gmail.com>
2018-03-16 20:10:37 -0700

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -4,3 +4,6 @@ function getRandomInt (min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+document.getElementById("randomBPM").innerHTML = getRandomInt;
+document.getElementById("minBPM").innerHTML = minBPM;
+document.getElementById("maxBPM").innerHTML = maxBPM;
