3ba186e173765b7432dfe247b8b337c9f413680e
Daphne Link <daphnelink@gmail.com>
2018-03-16 22:28:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function () {
     var maxBPM = 400;
     var minBPM = 10;
-    document.getElementById("randomBPM").innerHTML = Math.random() * (maxBPM - minBPM + 1) + minBPM;
+    document.getElementById("randomBPM").innerHTML = Math.floor(Math.random() * (maxBPM - minBPM + 1)) + minBPM;
 };
