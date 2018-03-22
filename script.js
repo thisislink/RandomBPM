f162ac0ccc066532867eb67738f2005658a77ec1
Daphne Link <daphnelink@gmail.com>
2018-03-21 19:53:31 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ function getBPM() {
     document.getElementById("randomBPM").innerHTML = Math.floor(Math.random() * (maxBPM - minBPM + 1)) + minBPM;
 }
 
+function getTimeSignature() {
+    var timeSignatures = [ '4/4', '3/4', '6/8'];   
+    document.getElementById("randomTimeSignatures").innerHTML = Math.floor(Math.random() * (timeSignatures + 1));
+}
+
+
 window.onload = function() {
     getBPM();
+    getTimeSignature();
 }
